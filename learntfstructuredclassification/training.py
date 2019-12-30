dcdbd87f7d8a4657aff1136e384828f56ada0868
Armeet Jatyani <xarmeetx@gmail.com>
2019-12-30 14:15:23 -0800

    working

--- learntfstructuredclassification/training.py
+++ learntfstructuredclassification/training.py
@@ -51,3 +51,88 @@ def df_to_dataset(dataframe, shuffle=True, batch_size=32):
     ds = ds.shuffle(buffer_size=len(dataframe))
   ds = ds.batch(batch_size)
   return ds
+
+batch_size = 5 # A small batch sized is used for demonstration purposes
+train_ds = df_to_dataset(train, batch_size=batch_size)
+val_ds = df_to_dataset(val, shuffle=False, batch_size=batch_size)
+test_ds = df_to_dataset(test, shuffle=False, batch_size=batch_size)
+
+# We will use this batch to demonstrate several types of feature columns
+example_batch = next(iter(train_ds))[0]
+logging.debug(example_batch)
+
+# A utility method to create a feature column
+# and to transform a batch of data
+def demo(feature_column):
+  feature_layer = layers.DenseFeatures(feature_column)
+  logging.debug(feature_layer(example_batch).numpy())
+
+age = feature_column.numeric_column("age")
+demo(age)
+
+age_buckets = feature_column.bucketized_column(age, boundaries=[18, 25, 30, 35, 40, 45, 50, 55, 60, 65])
+demo(age_buckets)
+
+thal = feature_column.categorical_column_with_vocabulary_list(
+      'thal', ['fixed', 'normal', 'reversible'])
+
+thal_one_hot = feature_column.indicator_column(thal)
+demo(thal_one_hot)
+
+# Notice the input to the embedding column is the categorical column
+# we previously created
+thal_embedding = feature_column.embedding_column(thal, dimension=8)
+demo(thal_embedding)
+
+thal_hashed = feature_column.categorical_column_with_hash_bucket(
+      'thal', hash_bucket_size=1000)
+demo(feature_column.indicator_column(thal_hashed))
+
+crossed_feature = feature_column.crossed_column([age_buckets, thal], hash_bucket_size=1000)
+demo(feature_column.indicator_column(crossed_feature))
+
+feature_columns = []
+
+# numeric cols
+for header in ['age', 'trestbps', 'chol', 'thalach', 'oldpeak', 'slope', 'ca']:
+  feature_columns.append(feature_column.numeric_column(header))
+# indicator cols
+thal = feature_column.categorical_column_with_vocabulary_list(
+      'thal', ['fixed', 'normal', 'reversible'])
+thal_one_hot = feature_column.indicator_column(thal)
+feature_columns.append(thal_one_hot)
+
+# embedding cols
+thal_embedding = feature_column.embedding_column(thal, dimension=8)
+feature_columns.append(thal_embedding)
+
+# crossed cols
+crossed_feature = feature_column.crossed_column([age_buckets, thal], hash_bucket_size=1000)
+crossed_feature = feature_column.indicator_column(crossed_feature)
+feature_columns.append(crossed_feature)
+
+feature_layer = tf.keras.layers.DenseFeatures(feature_columns)
+
+batch_size = 32
+train_ds = df_to_dataset(train, batch_size=batch_size)
+val_ds = df_to_dataset(val, shuffle=False, batch_size=batch_size)
+test_ds = df_to_dataset(test, shuffle=False, batch_size=batch_size)
+
+model = tf.keras.Sequential([
+  feature_layer,
+  layers.Dense(128, activation='relu'),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_ds,
+          validation_data=val_ds,
+          epochs=5)
+
+
+loss, accuracy = model.evaluate(test_ds)
+print("Accuracy", accuracy)
